1e1f1c0d72592392af63807aa1154550e4d8cbd6
ak131j <ak131j@att.com>
2021-08-10 02:02:40 +0530

    V1.0.3
    V1.0.3

--- Model/src/model/common/ModelUtil.java
+++ Model/src/model/common/ModelUtil.java
@@ -2,6 +2,8 @@ package model.common;
 
 import java.math.BigDecimal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 
 import java.util.Date;
@@ -13,14 +15,38 @@ public class ModelUtil {
     public ModelUtil() {
         super();
     }
-    
+
     public static BigDecimal getSequenceNextVal(String SeqName, DBTransaction transaction) {
         SequenceImpl seqImpl = new SequenceImpl(SeqName, transaction);
         return new BigDecimal(seqImpl.getSequenceNumber().toString());
     }
-    
+
     public static Timestamp getCurrentDate() {
         Date today = new Date();
         return new Timestamp(today.getTime());
+    }
+
+    public static String getSqlDescription(String sql, DBTransaction trans) {
+        PreparedStatement stat = null;
+        ResultSet rs = null;
+        try {
+            stat = trans.createPreparedStatement(sql, 1);
+            rs = stat.executeQuery();
+            while (rs.next()) {
+                return rs.getString(1);
+            }
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        } 
+        finally {
+            try {
+                rs.close();
+                stat.close();
+            } catch (Exception e) {
+                System.out.println("Error Closing the result set");
+            }
+        }
+        return "";
     }
 }
